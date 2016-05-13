970cf6b126d1b0de53a6f6eaece20e4c264682d0
Malte Pöggel <malte@poeggel.de>
2016-05-13 21:47:01 +0200

    Updated version number

--- firmware/user/cgiconfig.c
+++ firmware/user/cgiconfig.c
@@ -3,7 +3,7 @@
 // #############################################################################
 // # cgiconfig.c - CGI for "Sensor settings" and "User settings" tab           #
 // #############################################################################
-// #            Version: 1.0 - Compiler: esp-open-sdk 1.5.2 (Linux)            #
+// #            Version: 1.1 - Compiler: esp-open-sdk 1.5.2 (Linux)            #
 // #  (c) 2015-2016 by Malte Pöggel - www.MALTEPOEGGEL.de - malte@poeggel.de   #
 // #############################################################################
 // #  This program is free software; you can redistribute it and/or modify it  #
